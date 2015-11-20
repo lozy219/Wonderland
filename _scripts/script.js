90a75f39501205aa5fd0843bb72df7c1ddc7550f
Lei Mingyu <uygnim.iel@gmail.com>
2015-11-20 23:56:06 +0800

    loop forest sound

--- _scripts/script.js
+++ _scripts/script.js
@@ -56,6 +56,7 @@ var hideAndShow = function(wrapperToHide, wrapperToShow) {
     stopPlayAmbient();
 
     sound.forestAudio = new Audio('_sounds/forest.m4a');
+    sound.forestAudio.loop = true;
     sound.forestAudio.play();
   }
 
